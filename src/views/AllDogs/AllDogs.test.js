2f2d4999320ef4e3f7bfbdfb903466fdba6e35e5
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-04 17:18:38 -0800

    wrote passing test for all dogs js file

--- src/views/AllDogs/AllDogs.test.js
+++ src/views/AllDogs/AllDogs.test.js
@@ -1,8 +1,16 @@
-import AllDogs from './AllDogs';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import AllDogs from './AllDogs';
 
-test.skip('', () => {
-  const { conatiner } = render(<AllDogs />);
-  const nameElement = screen.getByText('Barton');
-  expect(nameElement).toBeInTheDocument();
+test('<AllDogs/> should render the entire list of dogs', async () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <Route path="/">
+        <AllDogs />
+      </Route>
+    </MemoryRouter>
+  );
+
+  await screen.findByText('Dogs Available For Adoption');
+  expect(container).toMatchSnapshot();
 });
