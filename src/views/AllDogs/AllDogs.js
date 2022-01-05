dd1898d2f450a65af537b3ea90912984c7c62b71
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-04 18:27:24 -0800

    added loading state for all dogs js

--- src/views/AllDogs/AllDogs.js
+++ src/views/AllDogs/AllDogs.js
@@ -5,14 +5,18 @@ import './AllDogs.css';
 
 export default function AllDogs() {
   const [dogs, setDogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchDogs();
       setDogs(data);
+      setLoading(false);
     };
     fetchData();
   }, []);
+
+  if (loading) return <h3>Loading list of doggies...</h3>;
 
   return (
     <div className="home">
