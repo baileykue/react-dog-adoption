8b6e173d7b7cf4224c3d5882231b7fde6bf351e8
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-05 14:44:15 -0800

    styled the detail page buttons

--- src/components/DogDetail/DogDetail.js
+++ src/components/DogDetail/DogDetail.js
@@ -3,8 +3,8 @@ import './DogDetail.css';
 
 export default function DogDetail({ name, bio, image }) {
   return (
-    <div>
-      <div className="detail">
+    <div className="detail">
+      <div className="bio-detail">
         <img src={image} className="bio-img" />
         <h2>{name}</h2>
         <p>{bio}</p>
