58f52b5c1f6d84636c1bf5f8b4b2212e662918ab
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-04 17:31:51 -0800

    added font and styling to the header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AllDogs from './views/AllDogs/AllDogs';
 import Dog from './views/Dog/Dog';
 import Header from './components/Header/Header';
+import background from './background.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <BrowserRouter>
         <Header />
 
