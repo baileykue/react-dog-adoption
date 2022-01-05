9a4503adfaa0e885fef0c4f3775dc3df062f02aa
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-04 18:21:32 -0800

    added final stylings to the home page

--- src/components/DogList/DogList.js
+++ src/components/DogList/DogList.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import DogCard from '../DogCard/DogCard';
+import './DogList.css';
 
 export default function DogList({ dogs }) {
   return (
-    <>
-      <h1>Dogs Available For Adoption</h1>
+    <div className="dogs">
       {dogs.map((dog) => (
         <Link key={dog.id} to={`/dogs/${dog.id}`}>
           <DogCard key={dog.id} {...dog} />
         </Link>
       ))}
-    </>
+    </div>
   );
 }
--- src/views/AllDogs/AllDogs.js
+++ src/views/AllDogs/AllDogs.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { fetchDogs } from '../../services/dogs';
 import DogList from '../../components/DogList/DogList';
+import './AllDogs.css';
 
 export default function AllDogs() {
   const [dogs, setDogs] = useState([]);
@@ -14,7 +15,8 @@ export default function AllDogs() {
   }, []);
 
   return (
-    <div className="dogs">
+    <div className="home">
+      <h1>Dogs Available For Adoption</h1>
       <DogList dogs={dogs} />
     </div>
   );
